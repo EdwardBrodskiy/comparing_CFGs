4e01c002f3271b4fdc71252e5eabf50e07663141
EdwardBrodskiy <49645295+EdwardBrodskiy@users.noreply.github.com>
2021-05-28 13:20:01 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+from nltk import *
+
+
+def words_of_depth(depth, alphabet):
+    for letter in alphabet:
+        if depth == 0:
+            yield letter
+        else:
+            for sub_word in words_of_depth(depth - 1, alphabet):
+                yield [letter, *sub_word]
+
+
+def is_matching_cfg(a, b, alphabet, max_depth: int):
+    parser_a = RecursiveDescentParser(a)
+
+    parser_b = RecursiveDescentParser(b)
+    for depth in range(max_depth):
+        print(depth)
+        for word in words_of_depth(depth, alphabet):
+
+            if (len(list(parser_a.parse(word))) == 0) != (len(list(parser_b.parse(word))) == 0):
+                print("CFG's don't for word", ''.join(word))
+                return False
+    return True
+
+
+letters = ['0', '1']
+
+grammar1 = """
+S -> X S X | Y S Y | X | Y
+X -> '0'
+Y -> '1'
+"""
+
+grammar2 = """
+S -> X S X | Y S Y | X | Y
+X -> '0'
+Y -> '1'
+"""
+
+cfg1 = CFG.fromstring(grammar1)
+
+cfg2 = CFG.fromstring(grammar2)
+
+print(is_matching_cfg(cfg1, cfg2, letters, 10))
