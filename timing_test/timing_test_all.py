a32a1a14baf14b839e0157f6d956425e12e45556
EdwardBrodskiy <49645295+EdwardBrodskiy@users.noreply.github.com>
2021-10-22 15:16:50 +0100

    added basic high level options to timing test

--- timing_test/timing_test_all.py
+++ timing_test/timing_test_all.py
@@ -6,6 +6,11 @@ from cfg import type_is_matching_cfg
 from typing import Dict, Callable, Union, Any
 from tools import read_gram_file, convert_to_cnf
 
+# Global test settings
+MAX_DEPTH = 2
+NUMBER_OF_CFGS_TO_TEST = 3
+USE_PAST_RESULTS = False
+
 
 def main():
     timing_test_time_all = TimeAll(
@@ -17,14 +22,20 @@ def main():
 class TimeAll(Timer):
     def __init__(self, *args, **kwargs):
         os.chdir(os.path.join('..', 'benchmarks'))
-        gram_files = list(filter(lambda f: '.gram' == f[-5:], os.listdir()))
-
+        global NUMBER_OF_CFGS_TO_TEST
+        if NUMBER_OF_CFGS_TO_TEST != 0:
+            gram_files = list(filter(lambda f: '.gram' == f[-5:], os.listdir()))[:NUMBER_OF_CFGS_TO_TEST]
+        else:
+            gram_files = list(filter(lambda f: '.gram' == f[-5:], os.listdir()))
+            NUMBER_OF_CFGS_TO_TEST = 'all'
         algorithms: Dict[str, type_is_matching_cfg] = {
-            'my_cyk_numpy': my_cyk_numpy.is_matching_cfg,
+            # 'my_cyk_numpy': my_cyk_numpy.is_matching_cfg,
             'my_cyk_memo': my_cyk_memo.is_matching_cfg,
             # 'my_cfg_analyzer': my_cfg_analyzer.is_matching_cfg
         }
-        super().__init__(TimerSettings('time_all', save_location=('..', 'timing_test', 'results'), re_build_table=True), gram_files,
+
+        super().__init__(TimerSettings(F'time_{NUMBER_OF_CFGS_TO_TEST}', save_location=('..', 'timing_test', 'results'),
+                                       re_build_table=USE_PAST_RESULTS), gram_files,
                          algorithms, *args, **kwargs)
 
     @staticmethod
@@ -39,12 +50,12 @@ class TimeAll(Timer):
 
     @staticmethod
     def generate_varying_input_data_for_test():
-        for i in range(1, 2):
+        for i in range(1, MAX_DEPTH + 1):
             yield i, {'depth': i}
 
     @staticmethod  # TODO: may be able to expand out the cnf and depth variables
     def algorithm_wrapper(algorithm: type_is_matching_cfg, **kwargs) -> Callable[[], bool]:
-        return lambda: algorithm(kwargs['cnf'], kwargs['cnf'], kwargs['alphabet'], kwargs['depth'])
+        return lambda: algorithm(kwargs['cnf'], kwargs['start'], kwargs['cnf'], kwargs['start'], kwargs['alphabet'], kwargs['depth'])
 
     def add_result_to_results(self, run_index: int, algorithm_name: str, result: Union[str, float], **input_data):
         self.results[algorithm_name][input_data['depth'] - 1] += result
