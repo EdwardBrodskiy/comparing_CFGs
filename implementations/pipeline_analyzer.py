5eca7b9ed6b5752aa2fd1272d5147ccaf9758106
EdwardBrodskiy <49645295+EdwardBrodskiy@users.noreply.github.com>
2021-11-09 12:49:26 +0000

    added rhs rule analyzer and created a more robust pipeline controller

--- implementations/pipeline_analyzer.py
+++ implementations/pipeline_analyzer.py
@@ -8,9 +8,30 @@ from implementations.my_cyk_numpy import parse
 
 from implementations.my_cyk_numpy import is_matching_cfg as numpy_is_matching_cfg
 
-pipeline_data = Dict[str, Any]
 
-pipeline_function = Callable[[CFG, CFG, int, pipeline_data], Optional[bool]]
+class Pipeline:
+    def __init__(self, a: CFG, b: CFG, max_depth: int):
+        self._a = a
+        self._b = b
+        self.max_depth = max_depth
+
+        self._list_rules = None
+
+        self.data: Dict[str, np.ndarray] = {}
+
+    @property
+    def list_rules(self):
+        if self._list_rules is not None:
+            return self._list_rules
+        self._list_rules = convert_cnf_to_list(self._a), convert_cnf_to_list(self._b)
+        return self._list_rules
+
+
+'''
+INPUT: 2 CFGs that are to be compared a maximum depth and work of prior pipes
+RETURN: Give back True or False if analysis was decisive otherwise return None 
+'''
+pipeline_function = Callable[[CFG, CFG, int, Pipeline], Optional[bool]]
 
 
 def main():
@@ -29,12 +50,13 @@ def is_matching_cfg(a: CFG, b: CFG, max_depth: int):
     return is_matching_cfg_pipelined(a, b, max_depth,
                                      [
                                          is_matching_alphabet,
+                                         match_rhs_lengths,
                                          lambda *args: numpy_is_matching_cfg(*args[:-1])
                                      ])
 
 
 def is_matching_cfg_pipelined(a: CFG, b: CFG, max_depth: int, pipeline: List[pipeline_function]):
-    data: pipeline_data = {}
+    data = Pipeline(a, b, max_depth)
     for pipe in pipeline:
         decision = pipe(a, b, max_depth, data)
         if decision is not None:
@@ -42,9 +64,23 @@ def is_matching_cfg_pipelined(a: CFG, b: CFG, max_depth: int, pipeline: List[pip
     return True
 
 
-def is_matching_alphabet(a: CFG, b: CFG, max_depth: int, pipeline) -> Optional[bool]:
+def is_matching_alphabet(a: CFG, b: CFG, max_depth: int, pipeline: Pipeline) -> Optional[bool]:
     if a.alphabet != b.alphabet:
         return False
+    return None
+
+
+def match_rhs_lengths(a: CFG, b: CFG, max_depth: int, pipeline: Pipeline) -> Optional[bool]:
+    list_rules_a, list_rules_b = pipeline.list_rules
+
+    table = np.zeros([len(list_rules_a), len(list_rules_b)], dtype=np.float16)
+
+    for rule_a_index, rhs_a in enumerate(list_rules_a):
+        for rule_b_index, rhs_b in enumerate(list_rules_b):
+            match = min(len(rhs_a), len(rhs_b)) / max(len(rhs_a), len(rhs_b))
+            table[rule_a_index, rule_b_index] = match
+
+    pipeline.data['rhs_lengths'] = table
     return None
 
 
