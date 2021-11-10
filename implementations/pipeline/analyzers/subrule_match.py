8eca3a7b1a0c60dbc494e5f362d2507c06c973a1
EdwardBrodskiy <49645295+EdwardBrodskiy@users.noreply.github.com>
2021-11-10 21:50:22 +0000

    added in the old analyzer algorithm into the pipeline as well as its optimized version and continued testing on them

---
+++ implementations/pipeline/analyzers/subrule_match.py
@@ -0,0 +1,65 @@
+from typing import List, Optional
+import numpy as np
+
+from cfg import cfg_rhs, CFG
+
+from implementations.pipeline.pipeline import Pipeline
+
+
+class UnknownValueError(Exception):
+    pass
+
+
+def match_subrules(a: CFG, b: CFG, max_depth: int, pipeline: Pipeline) -> Optional[bool]:
+    list_rules_a, list_rules_b = pipeline.list_rules
+    pipeline.data['sub-rules'] = generate_similarity_table_by_value_approach(list_rules_a, list_rules_b)
+    return None
+
+
+def generate_similarity_table_by_value_approach(a, b):
+    """
+    Approach solution by continuously re calculating the similarity table starting with everything at 0
+    """
+    table = np.zeros([len(a), len(b)], dtype=np.float16)
+    for _ in range(3):  # TODO: change this to check for how much the change happened
+        for rule_a_index in range(1, len(a)):
+            for rule_b_index in range(1, len(b)):
+                rhs_a = a[rule_a_index]
+                rhs_b = b[rule_b_index]
+                table[rule_a_index, rule_b_index] = get_match_score(table, rhs_a, rhs_b, False)
+
+    table[0, 0] = get_match_score(table, a[0], b[0], True)
+
+    return table
+
+
+def get_match_score(table, rule_a: cfg_rhs, rule_b: cfg_rhs, cheat: bool) -> float:
+    if len(rule_a) > len(rule_b):
+        return get_match_score_ls(table, rule_a, rule_b, cheat)
+    return get_match_score_ls(table, rule_b, rule_a, cheat)
+
+
+def get_match_score_ls(table, larger_rule, smaller_rule, cheat):
+    matches: List[float] = [0 for _ in larger_rule]
+
+    for index, rhs_rule_a in enumerate(larger_rule):
+        for rhs_rule_b in smaller_rule:
+            match = get_rhs_rule_match_score(table, rhs_rule_a, rhs_rule_b, cheat)
+            matches[index] = max(match, matches[index])
+
+    return sum(matches) / len(matches)
+
+
+def get_rhs_rule_match_score(table, rule_a, rule_b, cheat) -> float:
+    if type(rule_a) == type(rule_b):
+        if type(rule_a) is tuple:
+            left = table[rule_a[0], rule_b[0]]
+            right = table[rule_a[1], rule_b[1]]
+            if not cheat and (left < 0 or right < 0):
+                raise UnknownValueError
+            left, right = abs(left), abs(right)  # TODO: this assumes it is -1 if not defined
+            return left * right
+        else:
+            if rule_a == rule_b:
+                return 1
+    return 0
--- implementations/pipeline/pipeline_analyzer.py
+++ implementations/pipeline/pipeline_analyzer.py
@@ -1,17 +1,22 @@
-from typing import Tuple
+from typing import Tuple, Optional
 
-from cfg import cnf_10palindrome
+from cfg import CFG, cnf_10palindrome
+
 from implementations.my_cyk_numpy import is_matching_cfg as numpy_is_matching_cfg
-from implementations.pipeline.pipeline import pipeline_function
-from implementations.pipeline.analyzers import *
-from implementations.pipeline.comparators import *
+from implementations.pipeline.pipeline import pipeline_function, Pipeline
+
+from implementations.pipeline.analyzers.rhs_lengths import match_rhs_lengths
+from implementations.pipeline.analyzers.subrule_match import match_subrules
+from implementations.pipeline.analyzers.subrule_match_optimized import match_subrules as match_subrules_opt
+
+from implementations.pipeline.comparators.alphabet_match import is_matching_alphabet
 
 
 def main():
     import cProfile
     import pstats
     from tools import convert_to_cnf, read_gram_file
-    start, cfg = read_gram_file(r'..\benchmarks\C11Grammar1-1-1.gram')
+    start, cfg = read_gram_file(r'..\..\benchmarks\C11Grammar1-1-1.gram')
     cnf = convert_to_cnf(start, cfg)
     with cProfile.Profile() as pr:
         is_matching_cfg(cnf, cnf, 1)
@@ -24,6 +29,9 @@ def is_matching_cfg(a: CFG, b: CFG, max_depth: int):
                                      (
                                          is_matching_alphabet,
                                          match_rhs_lengths,
+                                         match_subrules,
+                                         match_subrules_opt,
+                                         sanity_check_method,
                                          lambda *args: numpy_is_matching_cfg(*args[:-1])
                                      ))
 
@@ -35,6 +43,12 @@ def is_matching_cfg_pipelined(a: CFG, b: CFG, max_depth: int, pipeline: Tuple[pi
         if decision is not None:
             return decision
     return True
+
+
+def sanity_check_method(a: CFG, b: CFG, max_depth: int, pipeline: Pipeline) -> Optional[bool]:
+    table_diff = pipeline.data['sub-rules'] - pipeline.data['sub-rules-opt'][:-1, :-1]
+    print('average difference between sub rule analyzer and the optimized version', abs(table_diff).mean())
+    return None
 
 
 if __name__ == '__main__':
