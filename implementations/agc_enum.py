6dc6dce54d75a992bee05b547271870966187c17
EdwardBrodskiy <49645295+EdwardBrodskiy@users.noreply.github.com>
2022-02-02 12:35:24 +0000

    added tree count function

--- implementations/agc_enum.py
+++ implementations/agc_enum.py
@@ -1,14 +1,16 @@
 from cfg import CFG, cfg_rhs, cnf_10palindrome
 from typing import Union, Tuple
+from math import prod
 
 memo = {}
 
 
 def memory(f):
     def helper(*args, **kwargs):
-        if args not in memo:
-            memo[args] = f(*args, **kwargs)
-        return memo[args]
+        key = (f.__name__, args)
+        if key not in memo:
+            memo[key] = f(*args, **kwargs)
+        return memo[key]
 
     return helper
 
@@ -37,6 +39,23 @@ def enum(root: Tuple[str, ...], index: int, depth=100, cfg: CFG = None) -> Union
     return tuple(item for sublist in sub_enums for item in sublist)
 
 
+@memory
+def get_no_trees(root: Tuple[str, ...], max_depth, cfg: CFG = None) -> int:
+    if max_depth == 1:
+        return 1
+    if len(root) == 1:
+        key: str = root[0]
+        if key in cfg.alphabet:
+            return 1
+
+        expanded: cfg_rhs = cfg.rules[key]
+
+        sentential_forms = map(lambda rule: (rule,) if type(rule) is str else rule, expanded)
+        return sum(map(lambda sentential_form: get_no_trees(tuple(sentential_form), max_depth - 1, cfg=cfg), sentential_forms))
+
+    return prod(map(lambda sub_tree: get_no_trees(sub_tree, max_depth, cfg=cfg), root))
+
+
 def main():
     cfg = CFG(
         rules={
@@ -48,12 +67,13 @@ def main():
         alphabet={'a', 'b'}
     )
     results = dict()
-    for i in range(400):
+    for i in range(40):
         key = str(enum((cfg.start,), i, cfg=cfg))
         if key not in results:
             results[key] = 0
         results[key] += 1
     print(results, len(results))
+    print(get_no_trees(('S',), 166, cfg=cfg))
 
 
 if __name__ == '__main__':
